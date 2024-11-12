09fcc2bb29a4a3a91793a8d5b2f16313f1bb6dbe
saim-fabtechsol <saim.ishfaq@fabtechsol.com>
2024-11-12 16:37:43 +0500

    template

--- js/main.js
+++ js/main.js
@@ -75,3 +75,12 @@ $(document).ready(function () {
         $(this).find('.accordian-icon').toggleClass('active');
     });
 });
+
+function displayToggle() {
+    var x = document.getElementById('side-bar');
+    if (x.style.display === 'none' || x.style.display === '') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
